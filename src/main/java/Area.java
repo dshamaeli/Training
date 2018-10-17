9c4a4150c705aca8723eb2e01175bc4ba2793d96
Daniel Shamaeli <daniel.shamaeli@crowderconsult.co.uk>
2018-10-17 12:30:08 +0100

    Area getId bug fixed

--- src/main/java/Area.java
+++ src/main/java/Area.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 
 import org.slf4j.Logger;
@@ -13,181 +13,169 @@ import java.util.List;
 /**
  * An area is a geographical region used to logically group part of a water
  * network.
- * 
- * @author daniel.shamaeli
  *
+ * @author daniel.shamaeli
  */
 public class Area {
 
-//	private static final Logger LOG = LoggerFactory.getLogger(Area.class);
-	private Integer id;
-	private String name;
-	private AreaType type;
-	private Boolean isActive;
-	private List<AreaSummary> summaryList = new ArrayList<>();
-	private List<Meter> metersList = new ArrayList<>();
 
-	/**
-	 * Generates an area. An area is a geographical region used to logically
-	 * group part of a water network.
-	 * 
-	 * @param id
-	 *            Unique number that identifies the area.
-	 * @param name
-	 *            Display name for this area.
-	 * @param type
-	 *            Shows the area type. One of Hydraulic, Operational or Quality.
-	 * @param isActive
-	 *            flag to show if the area is active(true) or not(False).
-	 */
-	public Area(Integer id, String name, AreaType type, Boolean isActive) {
-		this.id = id;
-		this.name = name;
-		this.type = type;
-		this.isActive = isActive;
-	}
+    private Integer id;
+    private String name;
+    private AreaType type;
+    private Boolean isActive;
+    private List<AreaSummary> summaryList = new ArrayList<>();
+    private List<Meter> metersList = new ArrayList<>();
 
-	/**
-	 * returns ID of the area
-	 * 
-	 * @return id
-	 */
-	private Integer getId() {
-		return id;
-	}
+    /**
+     * Generates an area. An area is a geographical region used to logically
+     * group part of a water network.
+     *
+     * @param id       Unique number that identifies the area.
+     * @param name     Display name for this area.
+     * @param type     Shows the area type. One of Hydraulic, Operational or Quality.
+     * @param isActive flag to show if the area is active(true) or not(False).
+     */
+    public Area(Integer id, String name, AreaType type, Boolean isActive) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.isActive = isActive;
+    }
 
-	/**
-	 * set ID of the Area
-	 * 
-	 * @param id
-	 */
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    /**
+     * returns ID of the area
+     *
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
 
-	/**
-	 * returns name of the area
-	 * 
-	 * @return
-	 */
-	public String getName() {
-		return name;
-	}
+    /**
+     * set ID of the Area
+     *
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	/**
-	 * sets name of the area
-	 * 
-	 * @param name
-	 *            of the area
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
+    /**
+     * returns name of the area
+     *
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
 
-	/**
-	 * returns type of the area
-	 * 
-	 * @return type of the area
-	 */
-	public AreaType getType() {
-		return this.type;
-	}
+    /**
+     * sets name of the area
+     *
+     * @param name of the area
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	/**
-	 * sets type of the area
-	 * 
-	 * @param type
-	 *            one of Hydraulic, Operational or Quality
-	 */
-	public void setType(AreaType type) {
-		this.type = type;
-	}
+    /**
+     * returns type of the area
+     *
+     * @return type of the area
+     */
+    public AreaType getType() {
+        return this.type;
+    }
 
-	/**
-	 * 
-	 * 
-	 * @return <code>true</code> if area is active and <code>false</code>
-	 *         otherwise
-	 */
-	public Boolean isActive() {
-		return isActive;
-	}
+    /**
+     * sets type of the area
+     *
+     * @param type one of Hydraulic, Operational or Quality
+     */
+    public void setType(AreaType type) {
+        this.type = type;
+    }
 
-	/**
-	 * Activate or deactivate an area
-	 * 
-	 * @param isActive
-	 *            <code>true</code> to activate and <code>false</code> to
-	 *            deactivate an area
-	 */
-	public void setisActive(Boolean isActive) {
-		this.isActive = isActive;
-	}
+    /**
+     * @return <code>true</code> if area is active and <code>false</code>
+     * otherwise
+     */
+    public Boolean isActive() {
+        return isActive;
+    }
 
-	/**
-	 * Add summary to the area
-	 * 
-	 * @param total
-	 * @param max
-	 * @param min
-	 * @see AreaSummary
-	 */
-	public void addSummary(Double total, Double max, Double min) {
-		AreaSummary areaSummary = new AreaSummary(this.id, total, max, min);
-		summaryList.add(areaSummary);
-	}
+    /**
+     * Activate or deactivate an area
+     *
+     * @param isActive <code>true</code> to activate and <code>false</code> to
+     *                 deactivate an area
+     */
+    public void setisActive(Boolean isActive) {
+        this.isActive = isActive;
+    }
 
-	public List<AreaSummary> getSummary() {
-		return this.summaryList;
-	}
+    /**
+     * Add summary to the area
+     *
+     * @param total
+     * @param max
+     * @param min
+     * @see AreaSummary
+     */
+    public void addSummary(Double total, Double max, Double min) {
+        AreaSummary areaSummary = new AreaSummary(this.id, total, max, min);
+        summaryList.add(areaSummary);
+    }
 
-	/**
-	 * gives a list of Area Data for an Area between a provided start and end
-	 * date
-	 * 
-	 * @param start
-	 *            start date of the summary
-	 * @param end
-	 *            end date of the summary
-	 * @return list of Area Data
-	 */
-	public List<AreaSummary> getSummary(Date start, Date end) {
-		List<AreaSummary> list = new ArrayList<>();
-		Iterator<AreaSummary> iterator = summaryList.iterator();
-		while (iterator.hasNext()) {
-			AreaSummary summary = iterator.next();
-			Date date = summary.getDate();
-			if (date.after(start) && date.before(end)) {
-				list.add(summary);
-			}
-		}
-		return list;
-	}
+    public List<AreaSummary> getSummary() {
+        return this.summaryList;
+    }
 
-	@Override
-	public int hashCode() {
-		return this.id;
-	}
+    /**
+     * gives a list of Area Data for an Area between a provided start and end
+     * date
+     *
+     * @param start start date of the summary
+     * @param end   end date of the summary
+     * @return list of Area Data
+     */
+    public List<AreaSummary> getSummary(Date start, Date end) {
+        List<AreaSummary> list = new ArrayList<>();
+        Iterator<AreaSummary> iterator = summaryList.iterator();
+        while (iterator.hasNext()) {
+            AreaSummary summary = iterator.next();
+            Date date = summary.getDate();
+            if (date.after(start) && date.before(end)) {
+                list.add(summary);
+            }
+        }
+        return list;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (obj == null)
-			return false;
-		if (!(obj instanceof Area))
-			return false;
-		if (obj == this)
-			return true;
-		return this.getId() == ((Area) obj).getId();
-	}
+    @Override
+    public int hashCode() {
+        return this.id;
+    }
 
-	@Override
-	public String toString() {
-		return super.toString() + "\nID: " + this.id + "\nName: " + this.name + "\nArea Type:" + this.type
-				+ "\nActivation Status: " + this.isActive;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        if (!(obj instanceof Area))
+            return false;
+        if (obj == this)
+            return true;
+        return this.getId() == ((Area) obj).getId();
+    }
 
-	public List<Meter> getAllMeters() {
-		return this.metersList;
-	}
+    @Override
+    public String toString() {
+        return super.toString() + "\nID: " + this.id + "\nName: " + this.name + "\nArea Type:" + this.type
+                + "\nActivation Status: " + this.isActive;
+    }
+
+    public List<Meter> getAllMeters() {
+        return this.metersList;
+    }
 
 }
--- src/test/java/AreaTest.java
+++ src/test/java/AreaTest.java
@@ -7,193 +7,197 @@ import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AreaTest {
-	private Integer id = 123;
-	private String name = "Quality Area 1";
-	private AreaType type = AreaType.QUALITY;
-	private boolean active = true;
-	private Double total = 221.022;
-	private Double min = 41.006;
-	private Double max = 46.222;
+    private static final Logger LOG = LoggerFactory.getLogger(AreaTest.class);
+    private Integer id = 123;
+    private String name = "Quality Area 1";
+    private AreaType type = AreaType.QUALITY;
+    private boolean active = true;
+    private Double total = 221.022;
+    private Double min = 41.006;
+    private Double max = 46.222;
 
-	private Area createTestSubject() {
-		return new Area(id, name, type, active);
+    private Area createTestSubject() {
+        return new Area(id, name, type, active);
 
-	}
+    }
 
-	private Area createEmptyTestSubject() {
-		return new Area(null, null, null, null);
+    private Area createEmptyTestSubject() {
+        return new Area(null, null, null, null);
 
-	}
+    }
 
-	@Test
-	public void testGetId() throws Exception {
-		Area testSubject;
-		Integer result;
+    @Test
+    public void testGetId() throws Exception {
+        Area testSubject;
+        Integer result;
 
-		// default test
-		testSubject = createTestSubject();
-		result = testSubject.getId();
-		assertEquals(this.id, result);
-	}
+        // default test
+        testSubject = createTestSubject();
+        result = testSubject.getId();
+        LOG.trace(String.valueOf(result));
+        assertEquals(this.id, result);
+    }
 
-	@Test
-	public void testSetId() throws Exception {
-		Area testSubject;
-		Integer id = this.id;
-		Integer result;
+    @Test
+    public void testSetId() throws Exception {
+        Area testSubject;
+        Integer id = this.id;
+        Integer result;
 
-		// default test
-		testSubject = createEmptyTestSubject();
-		testSubject.setId(id);
-		result = testSubject.getId();
-		assertEquals(id, result);
-	}
+        // default test
+        testSubject = createEmptyTestSubject();
+        testSubject.setId(id);
+        result = testSubject.getId();
+        assertEquals(id, result);
+    }
 
-	@Test
-	public void testGetName() throws Exception {
-		Area testSubject;
-		String result;
+    @Test
+    public void testGetName() throws Exception {
+        Area testSubject;
+        String result;
 
-		// default test
-		testSubject = createTestSubject();
-		result = testSubject.getName();
-		assertEquals(this.name, result);
-	}
+        // default test
+        testSubject = createTestSubject();
+        result = testSubject.getName();
+        assertEquals(this.name, result);
+    }
 
-	@Test
-	public void testSetName() throws Exception {
-		Area testSubject;
-		String name = this.name;
-		String result;
+    @Test
+    public void testSetName() throws Exception {
+        Area testSubject;
+        String name = this.name;
+        String result;
 
-		// default test
-		testSubject = createEmptyTestSubject();
-		testSubject.setName(name);
-		result = testSubject.getName();
-		assertEquals(name, result);
-	}
+        // default test
+        testSubject = createEmptyTestSubject();
+        testSubject.setName(name);
+        result = testSubject.getName();
+        assertEquals(name, result);
+    }
 
-	@Test
-	public void testGetType() throws Exception {
-		Area testSubject;
-		AreaType result;
+    @Test
+    public void testGetType() throws Exception {
+        Area testSubject;
+        AreaType result;
 
-		// default test
-		testSubject = createTestSubject();
-		result = testSubject.getType();
-		assertEquals(this.type, result);
-	}
+        // default test
+        testSubject = createTestSubject();
+        result = testSubject.getType();
+        assertEquals(this.type, result);
+    }
 
-	@Test
-	public void testSetType() throws Exception {
-		Area testSubject;
-		AreaType type = this.type;
-		AreaType result;
-		// default test
-		testSubject = createEmptyTestSubject();
-		testSubject.setType(type);
-		result = testSubject.getType();
-		assertEquals(this.type, result);
-	}
+    @Test
+    public void testSetType() throws Exception {
+        Area testSubject;
+        AreaType type = this.type;
+        AreaType result;
+        // default test
+        testSubject = createEmptyTestSubject();
+        testSubject.setType(type);
+        result = testSubject.getType();
+        assertEquals(this.type, result);
+    }
 
-	@Test
-	public void testIsActive() throws Exception {
-		Area testSubject;
-		Boolean result;
+    @Test
+    public void testIsActive() throws Exception {
+        Area testSubject;
+        Boolean result;
 
-		// default test
-		testSubject = createTestSubject();
-		result = testSubject.isActive();
-		assertEquals(this.active, result);
-	}
+        // default test
+        testSubject = createTestSubject();
+        result = testSubject.isActive();
+        assertEquals(this.active, result);
+    }
 
-	@Test
-	public void testSetisActive() throws Exception {
-		Area testSubject;
-		Boolean result;
-		// default test
-		testSubject = createEmptyTestSubject();
-		testSubject.setisActive(this.active);
-		result = testSubject.isActive();
-		assertEquals(this.active, result);
-	}
+    @Test
+    public void testSetisActive() throws Exception {
+        Area testSubject;
+        Boolean result;
+        // default test
+        testSubject = createEmptyTestSubject();
+        testSubject.setisActive(this.active);
+        result = testSubject.isActive();
+        assertEquals(this.active, result);
+    }
 
-	@Test
-	public void testAddSummary() throws Exception {
-		Area testSubject;
-		Double total = this.total;
-		Double max = this.max;
-		Double min = this.min;
+    @Test
+    public void testAddSummary() throws Exception {
+        Area testSubject;
+        Double total = this.total;
+        Double max = this.max;
+        Double min = this.min;
 
-		// default test
-		testSubject = createTestSubject();
-		testSubject.addSummary(total, max, min);
-	}
+        // default test
+        testSubject = createTestSubject();
+        testSubject.addSummary(total, max, min);
+    }
 
-	@Test
-	public void testGetSummary() throws Exception {
-		Area testSubject;
-		List<AreaSummary> result;
+    @Test
+    public void testGetSummary() throws Exception {
+        Area testSubject;
+        List<AreaSummary> result;
 
-		// default test
-		testSubject = createTestSubject();
-		result = testSubject.getSummary();
-	}
+        // default test
+        testSubject = createTestSubject();
+        result = testSubject.getSummary();
+    }
 
-	@Test
-	public void testGetSummary_1() throws Exception {
-		Area testSubject;
-		Date start = null;
-		Date end = null;
-		List<AreaSummary> result;
+    @Test
+    public void testGetSummary_1() throws Exception {
+        Area testSubject;
+        Date start = null;
+        Date end = null;
+        List<AreaSummary> result;
 
-		// default test
-		testSubject = createTestSubject();
-		result = testSubject.getSummary(start, end);
-	}
+        // default test
+        testSubject = createTestSubject();
+        result = testSubject.getSummary(start, end);
+    }
 
-	@Test
-	public void testHashCode() throws Exception {
-		Area testSubject;
-		int result;
+    @Test
+    public void testHashCode() throws Exception {
+        Area testSubject;
+        int result;
 
-		// default test
-		testSubject = createTestSubject();
-		result = testSubject.hashCode();
-	}
+        // default test
+        testSubject = createTestSubject();
+        result = testSubject.hashCode();
+    }
 
-	@Test
-	public void testEquals() throws Exception {
-		Area testSubject;
-		Object obj = null;
-		boolean result;
+    @Test
+    public void testEquals() throws Exception {
+        Area testSubject;
+        Object obj = null;
+        boolean result;
 
-		// test 1
-		testSubject = createTestSubject();
-		obj = null;
-		result = testSubject.equals(obj);
-		Assert.assertEquals(false, result);
-	}
+        // test 1
+        testSubject = createTestSubject();
+        obj = null;
+        result = testSubject.equals(obj);
+        Assert.assertEquals(false, result);
+    }
 
-	@Test
-	public void testToString() throws Exception {
-		Area testSubject;
-		String result;
+    @Test
+    public void testToString() throws Exception {
+        Area testSubject;
+        String result;
 
-		// default test
-		testSubject = createTestSubject();
-		result = testSubject.toString();
-	}
+        // default test
+        testSubject = createTestSubject();
+        result = testSubject.toString();
+    }
 
-	@Test
-	public void testGetAllMeters() throws Exception {
-		Area testSubject;
-		List<Meter> result;
+    @Test
+    public void testGetAllMeters() throws Exception {
+        Area testSubject;
+        List<Meter> result;
 
-		// default test
-		testSubject = createTestSubject();
-		result = testSubject.getAllMeters();
-	}
+        // default test
+        testSubject = createTestSubject();
+        result = testSubject.getAllMeters();
+    }
 }
