03b360b3fb945dff2b95d2e2ae269586f4c9aef9
Daniel Shamaeli <daniel.shamaeli@crowderconsult.co.uk>
2018-10-30 12:54:30 +0000

    MeterRepositoryDB 50% done

--- src/main/java/MeterRepositoryJDBC.java
+++ src/main/java/MeterRepositoryJDBC.java
@@ -1,9 +1,10 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -13,13 +14,18 @@ import java.util.List;
  */
 public class MeterRepositoryJDBC implements MeterRepository {
     private static final Logger LOG = LoggerFactory.getLogger(MeterRepositoryJDBC.class);
-    private Statement statement = Database.getStatement();
-    private ResultSet result = null;
-    private List<Meter> list = new ArrayList<>();
+    private Connection connection;
 
-
-    public MeterRepositoryJDBC() throws SQLException {
+    {
+        try {
+            connection = Database.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
+
+    private ResultSet result = null;
+
 
     @Override
     public List<Meter> getAllMeters(Area area) {
@@ -30,13 +36,14 @@ public class MeterRepositoryJDBC implements MeterRepository {
 
     @Override
     public List<Meter> getOldMeter(Date date) {
+        java.sql.Date dateSQL = new java.sql.Date(date.getTime());
         List<Meter> list = new ArrayList<>();
-        String query = "select * from Meter where install_date < to_date('10-10-2018 10:10:10','dd-mm-yyyy HH:MI:SS')";
-//        String query = "select meter_id from meter where install_date < TO_TIMESTAMP(" + date + ",'DD-MON-RRHH24:MI:SS.FF')";
-//        String query = "select meter_id from meter where install_date <" + date;
-//        String query = "select * from meter";
+        String query = "select * from Meter where install_date < ?";
         try {
-            result = statement.executeQuery(query);
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setDate(1, dateSQL);
+
+            result = preparedStatement.executeQuery();
             while (result.next()) {
                 Integer meter_id = result.getInt("meter_id");
                 String name = result.getString("name");
@@ -46,11 +53,16 @@ public class MeterRepositoryJDBC implements MeterRepository {
                 String type = result.getString("measurment_data_type");
                 MeasurementDataType measurement_data_type = MeasurementDataType.convert(type);
                 Meter meter = new Meter(meter_id, name, meter_type_id, install_date, is_active, measurement_data_type);
-                LOG.info(meter.toString());
                 list.add(meter);
             }
         } catch (SQLException e) {
             LOG.error("Error", e);
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
         return list;
     }
--- src/test/java/MeterRepositoryJDBCTest.java
+++ src/test/java/MeterRepositoryJDBCTest.java
@@ -1,20 +1,20 @@
-import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.TimeZone;
+import java.util.Date;
+import java.util.List;
 
 
 public class MeterRepositoryJDBCTest {
     private static final Logger LOG = LoggerFactory.getLogger(AreaTest.class);
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         Integer id = 123;
         String name = "Quality Area 1";
         AreaType type = AreaType.QUALITY;
@@ -25,15 +25,18 @@ public class MeterRepositoryJDBCTest {
         Area area = new Area(id, name, type, active);
     }
 
-    @After
-    public void tearDown() throws Exception {
-    }
-
     @Test
-    public void createDataSource() throws SQLException, ParseException {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd-mm-yyyy'T'HH:mm:ssZ");
-        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
-        MeterRepositoryJDBC meterDB = new MeterRepositoryJDBC();
-        meterDB.getOldMeter(sdf.parse("10-10-2018T14:10:10-0000"));
+    public void createDataSource() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy'T'HH:mm:ss.SSSZ");
+        MeterRepositoryJDBC meterDB = null;
+        List<Meter> result = null;
+        try {
+            meterDB = new MeterRepositoryJDBC();
+            Date date = sdf.parse("01-11-2018T12:11:56.235-0700");
+            result = meterDB.getOldMeter(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        Assert.assertEquals(123, result.get(0).getId());
     }
 }
