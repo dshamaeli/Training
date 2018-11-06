a5dfa5052ee2ef0c74d71dac44ff025b661d44f7
Daniel Shamaeli <daniel.shamaeli@crowderconsult.co.uk>
2018-11-06 10:16:26 +0000

    Refactored according to database comments

--- src/test/java/MeterRepositoryJDBCTest.java
+++ src/test/java/MeterRepositoryJDBCTest.java
@@ -1,11 +1,12 @@
 package uk.co.crowderconsult;
 
-import org.junit.Assert;
-import org.junit.Before;
+import org.junit.*;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -17,15 +18,51 @@ public class MeterRepositoryJDBCTest {
     private static final Logger LOG = LoggerFactory.getLogger(AreaTest.class);
 
     private MeterRepositoryJDBC meterDB;
-    private List<Meter> result = null;
 
-    @Before
-    public void setUp() {
+    {
         try {
             meterDB = new MeterRepositoryJDBC(Database.getConnection());
         } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private List<Meter> result = null;
+
+    @BeforeClass
+    public static void setUp() {
+        try {
+
+            Connection connection = Database.getConnection();
+            String insertArea = "insert into area (area_id,name,area_type,is_active) values (321,'Quality Area 1','QUALITY','y')";//NON-NLS
+            String insertMeterType1 = "insert into meter_type (meter_type_id,type_name,max_size,max_flow,min_flow) values (1,'Inlet Meter', 50.00,50.00,20.00)";//NON-NLS
+            String insertMeterType2 = "insert into meter_type (meter_type_id,type_name,max_size,max_flow,min_flow) values (2,'Abstraction Meter',650.00,100.00,100.00)";//NON-NLS
+            String insertMeter1 = "insert into meter (meter_id,name,install_date,is_active,measurment_data_type,meter_type_id) values (123,'Quality Area 1 Meter', to_date('13-DEC-2017 6:56 PM', 'dd-mon-yyyy hh:mi PM'),'Y','FLOW',1)";//NON-NLS
+            String insertMeter2 = "insert into meter (meter_id,name,install_date,is_active,measurment_data_type,meter_type_id) values (753,'Quality Area 1 Meter', to_date('25-DEC-2017 5:56 PM', 'dd-mon-yyyy hh:mi PM'),'Y','FLOW',4)";//NON-NLS
+            String insertAreaMeterLookup1 = "insert into AREA_METER_LOOKUP (meter_id,area_id) values (123,321)";//NON-NLS
+            String insertAreaMeterLookup2 = "insert into AREA_METER_LOOKUP (meter_id,area_id) values (753,321)";//NON-NLS
+            String[] query = {insertArea, insertMeterType1, insertMeterType2, insertMeter1, insertMeter2, insertAreaMeterLookup1, insertAreaMeterLookup2};
+            for (String q : query) {
+                try (PreparedStatement preparedStatement = connection.prepareStatement(q)) {
+                    preparedStatement.executeUpdate();
+                } catch (SQLException e) {
+                    LOG.error("getAllMeters query SQLException", e);//NON-NLS
+                }
+            }
+        } catch (SQLException e) {
             LOG.error("Error", e); //NON-NLS
         }
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        String deleteArea = "delete from area where area_id=321";//NON-NLS
+        String deleteMeterType1 = "delete from meter_type where meter_type_id=1";//NON-NLS
+        String deleteMeterType2 = "delete from meter_type where meter_type_id=2";//NON-NLS
+        String deleteMeter1 = "delete from meter where meter_id=123";//NON-NLS
+        String deleteMeter2 = "delete from meter where meter_id=753";//NON-NLS
+        String deleteAreaMeterLookup1 = "delete from AREA_METER_LOOKUP where meter_id=123 and area_id=321 ";//NON-NLS
+        String deleteAreaMeterLookup2 = "delete from AREA_METER_LOOKUP where meter_id=753 and area_id=321 ";//NON-NLS
     }
 
     @Test
--- src/test/resources/database.sql
+++ src/test/resources/database.sql
@@ -219,35 +219,8 @@ alter table AREA_METER_LOOKUP
 alter table AREA_METER_LOOKUP
   add constraint AREA_METER_LOOKUP_$FK2 foreign key (AREA_ID)
   references AREA (AREA_ID);
+  ALTER USER nb quota unlimited on USERS;
 
-
--- enable user to enter rows
+-- enables user to enter rows
 
 ALTER USER nb quota unlimited on USERS;
-
---Add row to area
-insert into area (area_id,name,area_type,is_active) values (321,'Quality Area 1','QUALITY','y');
-insert into area (area_id,name,area_type,is_active) values (987,'Quality Area 2','QUALITY','N');
-insert into area (area_id,name,area_type,is_active) values (556,'Hydraulic Area 1','HYDRAULIC','Y');
-insert into area (area_id,name,area_type,is_active) values (864,'Hydraulic Area 1','HYDRAULIC','Y');
-insert into area (area_id,name,area_type,is_active) values (777,'Operational Area 1','OPERATIONAL','Y');
-insert into area (area_id,name,area_type,is_active) values (256,'Operational Area 2','OPERATIONAL','N');
-
---Add row to meter_type
-insert into meter_type (meter_type_id,type_name,max_size,max_flow,min_flow) values (1,'Inlet Meter', 50.00,50.00,20.00);
-insert into meter_type (meter_type_id,type_name,max_size,max_flow,min_flow) values (2,'Abstraction Meter',650.00,100.00,100.00);
-insert into meter_type (meter_type_id,type_name,max_size,max_flow,min_flow) values (3,'Distribution Meter',750.00,100.00,100.00 );
-insert into meter_type (meter_type_id,type_name,max_size,max_flow,min_flow) values (4,'Outlet Meter', 50.00,50.00,20.00);
-
---Add row to meter
-insert into meter (meter_id,name,install_date,is_active,measurment_data_type,meter_type_id) values (123,'Quality Area 1 Meter', to_date('13-DEC-2017 6:56 PM', 'dd-mon-yyyy hh:mi PM'),'Y','FLOW',1);
-insert into meter (meter_id,name,install_date,is_active,measurment_data_type,meter_type_id) values (753,'Quality Area 1 Meter', to_date('25-DEC-2017 5:56 PM', 'dd-mon-yyyy hh:mi PM'),'Y','FLOW',4);
-insert into meter (meter_id,name,install_date,is_active,measurment_data_type,meter_type_id) values (258,'Hydraulic Area 1 Meter', to_date('25-OCT-2018 10:56 AM', 'dd-mon-yyyy hh:mi PM'),'Y','PRESSURE',2);
-insert into meter (meter_id,name,install_date,is_active,measurment_data_type,meter_type_id) values (951,'Operationl Area 1 Meter', to_date('20-SEP-2018 1:56 PM', 'dd-mon-yyyy hh:mi PM'),'Y','LEVEL',4);
-
---Add row to belongs to
-
-insert into AREA_METER_LOOKUP (meter_id,area_id) values (123,321);
-insert into AREA_METER_LOOKUP (meter_id,area_id) values (753,321);
-insert into AREA_METER_LOOKUP (meter_id,area_id) values (258,556);
-insert into AREA_METER_LOOKUP (meter_id,area_id) values (951,777);
